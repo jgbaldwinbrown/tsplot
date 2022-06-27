1627d48b9bdad4062b4024731e82ff77c4a0f671
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2022-06-27 13:15:12 -0600

    separate

--- scripts/ts_separate.go
+++ scripts/ts_separate.go
@@ -9,6 +9,7 @@ type Flags struct {
 	SyncPath string
 	BedPath string
 	InfoPath string
+	OutputPrefix string
 }
 
 func GetFlags() Flags {
@@ -16,6 +17,7 @@ func GetFlags() Flags {
 	flag.StringVar(&f.SyncPath, "s", "", "Path to sync file with allele frequencies")
 	flag.StringVar(&f.BedPath, "b", "", "Path to bed file with regions to include")
 	flag.StringVar(&f.InfoPath, "i", "", "Path to info file with identities of sync columns")
+	flag.StringVar(&f.OutputPrefix, "o", "separated", "Prefix for output files")
 	flag.Parse()
 	return f
 }
@@ -38,7 +40,7 @@ func main() {
 		panic(err)
 	}
 
-	plottables := tsplot.ToSeparatePlottables(sync, bed, info, "separated")
+	plottables := tsplot.ToSeparatePlottables(sync, bed, info, f.OutputPrefix)
 
 	err = tsplot.PlotPlottables(plottables...)
 	if err != nil {
