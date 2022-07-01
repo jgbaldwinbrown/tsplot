b4b1b97b89f68c891b06ba1116b78ebe44edf02b
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2022-06-30 18:21:21 -0600

    fixed maxes

--- tsplot/maxes.go
+++ tsplot/maxes.go
@@ -1,25 +1,22 @@
-package main
+package tsplot
 
 import (
-	"fmt"
 	"os"
-	"bufio"
-	"github.com/jgbaldwinbrown/tsplot/tsplot"
 	"github.com/jgbaldwinbrown/accel/accel"
 )
 
 func PathsToMaxes(paths []string) ([]float64, error) {
 	var afsets []accel.Afs
-	for _, path := range paths
+	for _, path := range paths {
 		conn, err := os.Open(path)
 		if err != nil {
 			return nil, err
 		}
 		defer conn.Close()
 
-		p := tsplot.ReadPlottable(conn)
+		p := ReadPlottable(conn)
 
-		afs, err := tsplot.PlottableToAfs(p)
+		afs, err := PlottableToAfs(p)
 		if err != nil {
 			return nil, err
 		}
